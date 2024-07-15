c315373038508a08bf1b508f752f63a1671498ea
haketa2434 <TTC22D3170014@edu.tech.ac.jp>
2024-07-16 00:31:19 +0900

    haketa

--- Assets/SimpleSF/Scripts/ScenaManager.cs
+++ Assets/SimpleSF/Scripts/ScenaManager.cs
@@ -13,23 +13,23 @@ namespace SimpleSFSample
         public Image curtainImage;
         public string nextSceneName;
 
-        private void Start()
-        {
-            // 初期化処理（例：最初のシーンを設定）
-            if (sceneTransitionAnimator != null)
-            {
-                sceneTransitionAnimator.nextSceneName = nextSceneName;
-            }
-        }
+        //private void Start()
+        //{
+        //    // 初期化処理（例：最初のシーンを設定）
+        //    if (sceneTransitionAnimator != null)
+        //    {
+        //        sceneTransitionAnimator.nextSceneName = nextSceneName;
+        //    }
+        //}
 
-        public async void StartSceneTransition(float duration)
-        {
-            // キャンセルトークンを作成
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        //public async void StartSceneTransition(float duration)
+        //{
+        //    // キャンセルトークンを作成
+        //    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
-            // シーン遷移を開始
-            await sceneTransitionAnimator.ExitSceneTransitionAsync(duration, cancellationTokenSource.Token);
-        }
+        //    // シーン遷移を開始
+        //    await sceneTransitionAnimator.ExitSceneTransitionAsync(duration, cancellationTokenSource.Token);
+        //}
     }
 }
 

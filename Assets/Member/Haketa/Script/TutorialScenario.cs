a009d31d983f6a47856e6fd60d74833d9b931e8d
haketa2434 <TTC22D3170014@edu.tech.ac.jp>
2024-08-30 18:12:17 +0900

    haketa
    Add

---
+++ Assets/Member/Haketa/Script/TutorialScenario.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScenario: MonoBehaviour
+{
+    public Question question;
+    [SerializeField]
+    private CSVRerder _csvrerder;
+    [SerializeField]
+    private Text _text;
+    [SerializeField]
+    private Text _name;
+
+    public int math = 0;
+    [SerializeField]
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        ReadQuestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ReadQuestion();
+            ++math;
+            if ("understanding" == question.move)
+            {
+                anim.SetBool("isunder", true);
+                anim.SetBool("isusua", false);
+            }
+            else if ("think" == question.move)
+            {
+                anim.SetBool("isunder", false);
+                anim.SetBool("isthink", true);
+            }
+            else if ("usually" == question.move)
+            {
+                anim.SetBool("isthink", false);
+                anim.SetBool("isusua", true);
+            }
+            else if ("End" == question.move)
+            {
+                //ÉVÅ[ÉìëJà⁄
+            }
+        }
+    }
+
+    private void ReadQuestion()
+    {
+        question = _csvrerder.GetQuestion();
+        _text.text = question.bun;
+        _name.text = question.name;
+    }
+}
